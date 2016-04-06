39b1686840793fd0de629767dca75ceff382f4e4
Ivailo Korakov <ivailo98@gmail.com>
2016-04-06 17:43:09 +0300

    Added Utilities.java and textures/assets.png

---
+++ src/Utilities/Utilities.java
@@ -0,0 +1,14 @@
+/* ===================
+ *        INFO
+ * ===================
+ * In this class we can put methods and other
+ * stuff that we will need during the game development
+ * For example if we need a number generator or something
+ * of this kind we can put it here.
+ */
+
+package Utilities;
+
+public class Utilities {
+    //// TODO: 06-Apr-16 // Put stuff
+}
