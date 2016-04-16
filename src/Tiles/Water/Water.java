f2522175d39d8f90d183f408accaf5c27bbf099f
Tsvetely <cvetygetova@yahoo.com>
2016-04-16 23:14:17 +0300

    Revert "Override method"
    This reverts commit a726592621d169ed7e01dd19165094c3eecf88d8.

--- src/Tiles/Water/Water.java
+++ src/Tiles/Water/Water.java
@@ -3,15 +3,8 @@ package Tiles.Water;
 import Graphics.Assets;
 import Tiles.Tile;
 
-import java.awt.*;
-
 public class Water extends Tile{
     public Water(int id) {
         super(Assets.water, id);
-    }
-
-    @Override
-    public void render(Graphics graphics, int x, int y) {
-        super.render(graphics, x, y);
     }
 }
